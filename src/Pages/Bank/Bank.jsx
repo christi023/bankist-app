3f6b56a220583a3e2085b32ace5fcb1bf43a7ea2
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-07-13 14:12:30 +0200

    Main page that holds components for banking

--- src/Pages/Bank/Bank.jsx
+++ src/Pages/Bank/Bank.jsx
@@ -1,20 +1,38 @@
 import React from 'react';
+// Components
+import Movements from '../../Components/BankMovements/Movements';
+import Summary from '../../Components/BankSummary/Summary';
+import Transfers from '../../Components/BankTransfers/Transfers';
+import Loan from '../../Components/BankLoan/Loan';
+import Close from '../../Components/CloseAccount/Close';
+
 // style
 import './Bank.css';
 
 const Bank = () => {
   return (
-    <main className="main-bank">
-      <div className="balance">
-        <div>
-          <p className="balance__label">Current Balance</p>
-          <p className="balance__date">
-            As of <span className="date">05/03/2037</span>
-          </p>
+    <>
+      <main className="main-bank">
+        <div className="balance">
+          <div>
+            <p className="balance__label">Current Balance</p>
+            <p className="balance__date">
+              As of <span className="date">05/03/2037</span>
+            </p>
+          </div>
+          <p className="balance__value">0000€</p>
         </div>
-        <p className="balance__value">0000€</p>
-      </div>
-    </main>
+        <Movements />
+        <Summary />
+        <Transfers />
+        <Loan />
+        <Close />
+
+        <p className="logout-timer">
+          You will be logged out in <span class="timer">05:00</span>
+        </p>
+      </main>
+    </>
   );
 };
 

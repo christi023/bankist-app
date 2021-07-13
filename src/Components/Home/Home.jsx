1933fdf04ebf6d2d3ba6f639f04d92decc4e8fca
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-07-13 11:05:44 +0200

    Home background img

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+// Components
+import './Home.css';
+
+const Home = () => {
+  return (
+    <>
+      <div className="home">
+        <div className="banner">
+          <h1>Welcome to Bankist</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;

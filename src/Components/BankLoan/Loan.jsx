76488156ddb8dc796ff05a98cac8df9903064d5f
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-07-13 14:08:15 +0200

    Bank/Request a loan

---
+++ src/Components/BankLoan/Loan.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+//style
+import './Loan.css';
+
+const Loan = () => {
+  return (
+    <div className="operation operation--loan">
+      <h2>Request loan</h2>
+      <form className="form form--loan">
+        <input type="number" className="form__input form__input--loan-amount" />
+        <button className="form__btn form__btn--loan">&rarr;</button>
+        <label className="form__label form__label--loan">Amount</label>
+      </form>
+    </div>
+  );
+};
+
+export default Loan;
